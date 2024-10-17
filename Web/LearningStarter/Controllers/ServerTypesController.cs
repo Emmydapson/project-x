9c106f92fd7968f2d1764d9e1f019daf7553df02
angeliccodes <angel.njoku@selu.edu>
2024-10-17 16:51:02 -0500

    ChangedRouteName

--- Web/LearningStarter/Controllers/ServerTypesController.cs
+++ Web/LearningStarter/Controllers/ServerTypesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace LearningStarter.Controllers;
 
 [ApiController]
-[Route("api/ServerTypes")]
+[Route("api/server-types")]
 
 public class ServerTypesController : ControllerBase
 {
